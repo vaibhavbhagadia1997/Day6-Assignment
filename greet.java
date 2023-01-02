c174e2e6498b9bd3c9543c9e0d38caeb016fad3c
vaibhavbhagadia1997 <120355403+vaibhavbhagadia1997@users.noreply.github.com>
2023-01-03 00:19:33 +0530

    "Added:Greeting"

---
+++ greet.java
@@ -0,0 +1,11 @@
+package Day6Assignment;
+
+public class greet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("DAY 6 - ASSIGNMENT");
+	}
+
+}
